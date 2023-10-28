6208056195f95803c2ff6e020b01c57cb8ac8fc0
patrickpr86 <sh4dbr@gmail.com>
2023-10-28 17:19:43 -0300

    criando body da pr a partir de um template

---
+++ actions-demo/src/main/kotlin/com/sample/actionsdemo/ActionsDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.sample.actionsdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActionsDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ActionsDemoApplication>(*args)
+}
